4e612aff17b8d697ac704cbf252eb423d52748d3
javiergarza <jgarza@akamai.com>
2018-09-28 08:18:16 -0700

    updates

--- examples/lab-pd.sh
+++ examples/lab-pd.sh
@@ -7,16 +7,19 @@ fi
 echo -e "\nGenerating the list of commands..."
 echo -e "\nakamai promotional-deployment new-pipeline --pipeline ${UNIQUENAME} --propertyId ${UNIQUENAME}.world-tour dev prod"
 echo -e "\nakamai promotional-deployment set-default --pipeline ${UNIQUENAME}"
-for MYENV in dev prod ; do
-	# create backup files
-	echo -e "\nmv ${UNIQUENAME}/environments/${MYENV}/hostnames.json ${UNIQUENAME}/environments/${MYENV}/hostnames-original.json"
-	echo -e "\njq \".[].cnameFrom = \\\"${MYENV}.${UNIQUENAME}.world-tour.akamaideveloper.net\\\" | .[].cnameTo = \\\"world-tour.akamaideveloper.net.edgesuite.net\\\" | .[].edgeHostnameId = 3190586\" ${UNIQUENAME}/environments/${MYENV}/hostnames-original.json > ${UNIQUENAME}/environments/${MYENV}/hostnames.json"
-done
+
+echo -e "\nmv ${UNIQUENAME}/environments/dev/hostnames.json ${UNIQUENAME}/environments/dev/hostnames-original.json"
+echo -e "\njq \".[].cnameFrom = \\\"dev.${UNIQUENAME}.world-tour.akamaideveloper.net\\\" | .[].cnameTo = \\\"world-tour.akamaideveloper.net.edgesuite.net\\\" | .[].edgeHostnameId = 3190586\" ${UNIQUENAME}/environments/dev/hostnames-original.json > ${UNIQUENAME}/environments/dev/hostnames.json"
+echo -e "\nmv ${UNIQUENAME}/environments/prod/hostnames.json ${UNIQUENAME}/environments/prod/hostnames-original.json"
+echo -e "\njq \".[].cnameFrom = \\\"${UNIQUENAME}.world-tour.akamaideveloper.net\\\" | .[].cnameTo = \\\"world-tour.akamaideveloper.net.edgesuite.net\\\" | .[].edgeHostnameId = 3190586\" ${UNIQUENAME}/environments/prod/hostnames-original.json > ${UNIQUENAME}/environments/promotional-deployment/hostnames.json"
+
 echo -e "\nmv ${UNIQUENAME}/environments/variableDefinitions.json ${UNIQUENAME}/environments/variableDefinitions-original.json"
-echo -e "\njq \".definitions.originHostname.default = \\\"${UNIQUENAME}.origin.akamaideveloper.net\\\" | .definitions.cpCode.default = 749512\" ${UNIQUENAME}/environments/variableDefinitions-original.json > ${UNIQUENAME}/environments/variableDefinitions.json"
+echo -e "\njq \" .definitions.originAPIHostname.type = \\\"hostname\\\" | .definitions.cpCode.default = 749512 | .definitions.originAPIHostname.default = null\" ${UNIQUENAME}/environments/variableDefinitions-original.json > ${UNIQUENAME}/environments/variableDefinitions.json"
+
+
 for MYENV in dev prod ; do
 	echo -e "\nmv ${UNIQUENAME}/environments/${MYENV}/variables.json ${UNIQUENAME}/environments/${MYENV}/variables-original.json"
-	echo -e "\njq \".originHostname = \\\"${MYENV}.${UNIQUENAME}.origin.akamaideveloper.net\\\" \" ${UNIQUENAME}/environments/${MYENV}/variables-original.json > ${UNIQUENAME}/environments/${MYENV}/variables.json"
+	echo -e "\njq \".originHostname = \\\"origin-web.urbancrawlapp.com\\\" | .originAPIHostname = \\\"origin-api.urbancrawlapp.com\\\" \" ${UNIQUENAME}/environments/${MYENV}/variables-original.json > ${UNIQUENAME}/environments/${MYENV}/variables.json"
 done
 echo -e "\nakamai promotional-deployment list-status"
 echo -e "\nakamai promotional-deployment promote dev --network staging --emails $EMAIL"
