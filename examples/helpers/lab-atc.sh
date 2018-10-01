ef594ed5fd4d4d79b1c18ce70a5b8d1154af486e
javiergarza <jgarza@akamai.com>
2018-10-01 10:23:52 -0700

    updating atc lab

--- examples/helpers/lab-atc.sh
+++ examples/helpers/lab-atc.sh
@@ -6,9 +6,9 @@ else
 	source $CONFIG
 fi
 echo -e "\nGenerating the list of commands... (please ensure you edit the .json files before submitting the call, and replace <test_definition_id> and <test_definition_execution_id> with the values returned but the API calls)"
-echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: POST :/test-management/v1/test-definitions < ~/examples/create-test-definition.json"
-echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: POST :/test-management/v1/test-definitions/<test_definition_id>/test-cases < ~/examples/create-test-case.json"
-echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: POST :/test-management/v1/test-runs < ~/examples/test-run-obj.json"
+echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: POST :/test-management/v1/test-definitions < ~/examples/test-center/create-test-definition.json"
+echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: POST :/test-management/v1/test-definitions/<test_definition_id>/test-cases < ~/examples/test-center/create-test-case.json"
+echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: POST :/test-management/v1/test-runs < ~/examples/test-center/test-run-obj.json"
 echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: GET :/test-management/v1/test-definition-executions/<test_definition_execution_id>"
 echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: GET :/test-management/v1/test-definition-executions/<test_definition_execution_id>/differences"
-echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: PUT :/test-management/v1/test-definition-executions/<test_definition_execution_id>/differences < ~/examples/differences.json"
+echo -e "\nhttp --session world-tour --auth-type=edgegrid -a atc: PUT :/test-management/v1/test-definition-executions/<test_definition_execution_id>/differences < ~/examples/test-center/differences.json"
