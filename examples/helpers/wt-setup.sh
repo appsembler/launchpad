3d3c41481d770797f257285e5138d48a9bdd64d6
javiergarza <jgarza@akamai.com>
2018-10-14 20:29:53 -0700

    updates

--- examples/helpers/wt-setup.sh
+++ examples/helpers/wt-setup.sh
@@ -1,10 +1,8 @@
 CONFIG=$HOME/wt-setup.txt
-echo "We will ask you a few questions to generate the list of commands you need to run to complete this lab"
-echo -n "First name: " ; read FIRSTNAME
-echo -n "Last name: " ; read LASTNAME
-echo -n "City: " ; read CITY
+echo "Enter the unique name you are going to use. A good practice is your initial and last name, a dash and the city where you are. For example jsmith-sanfrancisco"
+echo -n "Unique name: " ; read UNIQUENAME
 #UNIQUENAME="`echo ${FIRSTNAME}|awk '{print tolower(substr ($0, 0, 1))}'``echo ${LASTNAME}| awk '{print tolower($1)}'`-`date +'%Y%m%d'`"
-UNIQUENAME="`echo ${FIRSTNAME}|awk '{print tolower(substr ($0, 0, 1))}'``echo ${LASTNAME}| awk '{print tolower($1)}'`-`echo ${CITY}| awk '{gsub(/ /, "", $0);print tolower($0)}'`"
+UNIQUENAME=`echo ${UNIQUENAME} | awk '{gsub(/ /, "", $0);print tolower($0)}'`
 echo "Your unique name is: ${UNIQUENAME}"
 echo "UNIQUENAME=${UNIQUENAME}" > $CONFIG
 echo -n "email (for receiving activation notifications): " ; read EMAIL
