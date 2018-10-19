3d3757bd8578964628c26e593d85342f64e2beb2
javiergarza <jgarza@akamai.com>
2018-10-19 10:35:25 -0700

    updates based on feedback

--- examples/helpers/get-im-policy-name.sh
+++ examples/helpers/get-im-policy-name.sh
@@ -1,4 +1,4 @@
-POLICYNAME=`akamai property retrieve $1 2> /dev/null|grep policyTokenDefault|sed 's/.*://;s/,//;s/"//g'`
+POLICYNAME=`akamai property retrieve $1 2> /dev/null|grep policyTokenDefault|sed 's/.*: //;s/,//;s/"//g'`
 ASSETID=`akamai property search $1 2> /dev/null | jq '.versions.items[0].assetId'|sed 's/.*_//;s/"//'`
 
 #echo POLICYNAME=${POLICYNAME}
--- examples/helpers/lab-im.sh
+++ examples/helpers/lab-im.sh
@@ -1,4 +1,5 @@
-CONFIG=$HOME/wt-setup.txt
+CONFIG=${HOME}/wt-setup.txt
+TEMPLATEPATH=${HOME}/examples/image-manager
 if [ ! -f $CONFIG ] ; then 
 	~/examples/helpers/wt-setup.sh
 	source $CONFIG
@@ -12,11 +13,13 @@ if [ `echo "$POLICYNAME" | awk '{print $1}'` = "ERROR:" ] ; then
 	exit 1
 else
 	echo -e "\nThe Image Manager Policy Set name for property ${PROPERTY} is: ${POLICYNAME}" 
+	echo -e "\nExporting Policy Set name into environment variable IM_TOKEN. You can run the command below to verify:\necho $IM_TOKEN"
+	export IM_TOKEN=$POLICYNAME
 fi
 echo -e "\nGenerating the list of commands..."
 echo -e "\nakamai image-manager --policy-set ${POLICYNAME} list-policies --network staging"
 echo -e "\nakamai image-manager --policy-set ${POLICYNAME} get-policy .auto --network staging"
-echo -e "\nakamai image-manager --policy-set ${POLICYNAME} set-policy .auto --input-file ~/examples/im_default.json --network staging"
-echo -e "\nakamai image-manager --policy-set ${POLICYNAME} set-policy watermark --input-file ~/examples/im_watermark.json --network staging"
-echo -e "\nakamai image-manager --policy-set ${POLICYNAME} set-policy avatar --input-file ~/examples/im_avatar.json --network staging"
+echo -e "\nakamai image-manager --policy-set ${POLICYNAME} set-policy .auto --input-file ${TEMPLATEPATH}/im_default.json --network staging"
+echo -e "\nakamai image-manager --policy-set ${POLICYNAME} set-policy watermark --input-file ${TEMPLATEPATH}/im_watermark.json --network staging"
+echo -e "\nakamai image-manager --policy-set ${POLICYNAME} set-policy avatar --input-file ${TEMPLATEPATH}/im_avatar.json --network staging"
 
