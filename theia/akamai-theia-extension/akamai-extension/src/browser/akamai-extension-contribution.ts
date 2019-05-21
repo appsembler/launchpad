fb14d269c5470e5f84497503b5a60e2bfd88f2ee
Davey Shafik <me@daveyshafik.com>
2019-05-21 10:07:06 -0700

    Open the URL in the query argument `page` if it exists, every time the IDE opens

--- theia/akamai-theia-extension/akamai-extension/src/browser/akamai-extension-contribution.ts
+++ theia/akamai-theia-extension/akamai-extension/src/browser/akamai-extension-contribution.ts
@@ -1,20 +1,24 @@
 import { injectable, inject } from "inversify";
 import { ILogger, MaybePromise, CommandRegistry, CommandContribution, MenuContribution, MenuModelRegistry, MAIN_MENU_BAR } from "@theia/core/lib/common";
 import URI from "@theia/core/lib/common/uri";
-import { FrontendApplication, FrontendApplicationContribution } from "@theia/core/lib/browser";
+import {CommonCommands, FrontendApplication, FrontendApplicationContribution} from "@theia/core/lib/browser";
 import { FileNavigatorContribution } from "@theia/navigator/lib/browser/navigator-contribution";
 import { TerminalService } from "@theia/terminal/lib/browser/base/terminal-service";
 import { open, OpenerService} from '@theia/core/lib/browser';
 import { WorkspaceService } from '@theia/workspace/lib/browser';
 import { PreviewContribution } from '@theia/preview/lib/browser/preview-contribution';
-// import { MiniBrowserCommands } from '@theia/mini-browser/lib/browser/mini-browser-open-handler';
+import { MiniBrowserCommands } from '@theia/mini-browser/lib/browser/mini-browser-open-handler';
 import { AkamaiPreferences } from './akamai-extension-preferences';
 import { CommonMenus } from '@theia/core/lib/browser/common-frontend-contribution';
+import {FrontendApplicationStateService} from "@theia/core/lib/browser/frontend-application-state";
 // import { EditorMainMenu } from '@theia/editor/lib/browser/editor-menu';
 
 
 @injectable()
 export class AkamaiExtensionFrontendApplicationContribution implements FrontendApplicationContribution {
+    @inject(FrontendApplicationStateService)
+    protected readonly stateService: FrontendApplicationStateService;
+
     constructor(
         @inject(ILogger) protected readonly logger: ILogger,
         @inject(FileNavigatorContribution) protected readonly navigator: FileNavigatorContribution,
@@ -30,8 +34,7 @@ export class AkamaiExtensionFrontendApplicationContribution implements FrontendA
     async onStart(app: FrontendApplication) {
         // Layout doesn't need to be initialized before we can start a terminal
         let terminalWidget = await this.terminal.newTerminal({});
-        terminalWidget.start();
-        this.terminal.activateTerminal(terminalWidget);
+        terminalWidget.start().then(a => this.terminal.open(terminalWidget));
         this.menus.unregisterMenuAction("git-history:open-branch-history", CommonMenus.VIEW_VIEWS);
         this.menus.unregisterMenuAction("problemsView:toggle", CommonMenus.VIEW_VIEWS);
         this.menus.unregisterMenuAction("callhierachy:toggle", CommonMenus.VIEW_VIEWS);
@@ -43,6 +46,18 @@ export class AkamaiExtensionFrontendApplicationContribution implements FrontendA
         this.menus.unregisterMenuAction("workspace:close", CommonMenus.FILE_CLOSE);
         this.menus.unregisterMenuAction("workspace:saveAs", CommonMenus.FILE_OPEN);
         this.menus.unregisterMenuAction(CommonCommands.ABOUT_COMMAND);
+
+        if (!this.workspaceService.opened) {
+            this.stateService.reachedState('ready').then(
+                a => {
+                    // opens a website in preview
+                    let url = this.getQueryparam("page");
+                    if (url !== "" && url !== null) {
+                        this.commands.executeCommand(MiniBrowserCommands.OPEN_URL.id, url);
+                    }
+                }
+            );
+        }
     }
 
     initializeLayout(app: FrontendApplication): MaybePromise<void> {
@@ -52,11 +67,8 @@ export class AkamaiExtensionFrontendApplicationContribution implements FrontendA
         // We open a new file view in the navigator
         this.navigator.openView({
             activate: true
-        })
+        });
         
-        // opens a website in preview
-        // this.commands.executeCommand(MiniBrowserCommands.OPEN_URL.id, "https://www.bostonglobe.com/")
-
         // We get the path of the currently open folder in the workspace (passed when starting the IDE)
         // and open the README.md file in that folder
         const workspace = this.workspaceService.workspace;
@@ -69,6 +81,16 @@ export class AkamaiExtensionFrontendApplicationContribution implements FrontendA
             // );
   
         }
+    }
+
+    protected getQueryparam(name: string, url?: string): string | null {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
 }
 
